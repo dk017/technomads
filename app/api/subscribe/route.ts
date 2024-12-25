c235a70bf4cd3d3bab4ff99e20307d9fc2412027
dk017 <dhineshkumar.stoic@gmail.com>
2024-12-25 11:33:18 +0530

    removed not so needed files

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@/app/utils/supabase/server';
 import { NextResponse } from 'next/server';
+export const runtime = 'edge';
 
 export async function POST(request: Request) {
   try {
