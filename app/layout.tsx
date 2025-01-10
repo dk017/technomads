060efd22be60e792024f907465f968c6077d9587
dk017 <dhineshkumar.stoic@gmail.com>
2025-01-10 16:44:13 +0530

    Save subscription-based features for future implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,18 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { EmailCaptureWrapper } from "@/components/EmailCaptureWrapper";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Only Remote Jobs",
+  description: "Only Remote Jobs",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
